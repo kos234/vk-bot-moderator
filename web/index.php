dd3f0f18e4e5ffdf9b242899b3ffc29d0a9d9b52
kos234 <kostyaperfiliev94@gmail.com>
2020-05-21 02:02:47 +0800

    start

--- web/index.php
+++ web/index.php
@@ -41,8 +41,11 @@ switch ($data->type) {
         break;
 
         case 'message_new':
+            if($data->object->message->peer_id != $data->object->message->from_id)
+                createTabs($data->object->message->peer_id, $mysqli);
+
             $request_params = array(
-                'message' => "Я ебал этот PHP" , //сообщение
+                'message' => "" , //сообщение
                 'access_token' => TOKEN_VK_BOT, //токен для отправки от имени сообщества
                 'peer_id' => $data->object->message->peer_id, //айди чата
                 'random_id' => 0, //0 - не рассылка
@@ -53,6 +56,42 @@ switch ($data->type) {
             );
 
             $vk = new VK\Client\VKApiClient();
+            $text = explode(' ', $data->object->text);
+
+            if (strcasecmp($text[0], "/info") == 0 || strcasecmp($text[0], "/") == 0 || strcasecmp($text[0], "/инфо") == 0 || strcasecmp($text[0], "/инфа") == 0) {
+                $request_params['message'] = "&#129302;Bot moderator by kos v2.0.0\n\n"
+                    . "&#9999;Команды:\n"
+                    . "&#128196;/Info|Инфо — информация о боте\n"
+                    . "/Info user|Инфо пользователя {@Айди|@домен|Пересланое сообщение} — информация пользователя в вк и чате\n"
+                    . "/Сократить ссылку {ссылка} — сокращает ссылку через сервис вк\n"
+                    . "/Инвайт ссылка|Приглашение|Ссылка приглашение - выводит ссылку на приглашение в этот чат\n"
+                    . "/Пригласить {@Айди|@домен|Пересланое сообщение} [Сообщение] - отправляет приглашение пользователю в этот чат\n"
+                    . "/Список {Пользователей|забаненных|вышедших} - выводит указанный список пользователей\n"
+                    . "/Неактив - выводит список неактивных пользователей\n"
+                    . "/Онлайн - выводит список пользователей онлайн\n\n"
+                    . "Модерация:\n"
+                    . "/Предупреждение|Пред {@Айди|@домен|Пересланое сообщение} [Количество] [Причина] - Выдать предупреждение\n"
+                    . "/Кик|Исключить {@Айди|@домен|Пересланое сообщение} [Причина] - Исключить пользователя из чата\n"
+                    . "/Временный бан {@Айди|@домен|Пересланое сообщение} {Время SS:MM:HH:DDD:MM} [Причина] - Временно забанить пользователя в беседе\n"
+                    . "/Бан {@Айди|@домен|Пересланое сообщение} [Причина] - Забанить пользователя\n"
+                    . "/Разбанить|Пардон {@Айди|@домен|Пересланое сообщение} [Причина] - Разбанить пользователя\n"
+                    . "/Мега кик|мега исключение {Неактивных|вышедших|пользователей} - исключает пользователей из определённой группы\n"
+                    . "{} - обязательный параметр, [] - необязательный параметр, {] - тип зависит от задачи\n\n"
+                    . "&#9881;Настройки:\n"
+                    . "/Лимит повышение рангов {Уровень 1 - 5} {Количество предупреждений} {Количество киков} {Количество временных баннов} - устанавливает лимит повышение рангов модераторам\n"
+                    . "/Наказания за предупреждения {Тип: кик, временный бан, бан} {Количество} {Время, если тип: временный бан] - Установить наказание за достижение определенного количества предупреждений\n"
+                    . "/Очистить таблицу {Пользователей|забаненных|вышедших|модераторов|наказаний|всё} - очищает указанную таблицу\n"
+                    . "/Авто очистка предупреждений {Время SS:MM:HH:DDD:MM} - сбрасывает всё предупреждения через указанное время\n"
+                    . "/Приветствие {Текст} - Установить приветствие для новых пользователей\n"
+                    . "/Сообщать о наказаниях {@Айди|@домен|Пересланое сообщение} - люди, которым приходят уведомления о выдачи наказаний(если людей несколько, указывать через запятую без пробелов)\n"
+                    . "/Автокик|автоисключение {Включить|выключить|on|off} - Автоисключение вышедших пользователей\n\n"
+                    . "&#128214;Информация о проекте:\n"
+                    . "&#128100;Создатель: https://vk.com/i_love_python\n"
+                    . "&#128064;Исходные код проекта и гайд по подключению: https://github.com/kos234/Vk-bot-moderator\n";
+            }
+
+
+
 
             $vk->messages()->send(TOKEN_VK_BOT, $request_params);
 
@@ -60,4 +99,18 @@ switch ($data->type) {
         break;
 
 }
+
+function createTabs($chat_id, $mysqli){
+    $mysqli->query("CREATE TABLE IF NOT EXISTS `". $chat_id ."_users`(`id` VarChar( 255 ) NOT NULL, `mes_count` Int( 255 ) NOT NULL DEFAULT 0, `rang` TinyInt( 1 ) NOT NULL DEFAULT 0 ) ENGINE = InnoDB;");
+    $mysqli->query("CREATE TABLE IF NOT EXISTS `". $chat_id ."_punishments`(`id` VarChar( 255 ) NOT NULL, `type` VarChar( 255 ) NOT NULL, `text` VarChar( 255 ) NOT NULL, `parametr` Int( 255 ) NOT NULL ) ENGINE = InnoDB;");
+    $mysqli->query("CREATE TABLE IF NOT EXISTS `". $chat_id ."_moders`(`id` VarChar( 255 ) NOT NULL, `bans` Int( 255 ) NOT NULL DEFAULT 0, `kicks` Int( 255 ) NOT NULL DEFAULT 0, `tempbans` Int( 255 ) NOT NULL DEFAULT 0, `preds` Int( 255 ) NOT NULL DEFAULT 0, CONSTRAINT `unique_id` UNIQUE( `id` ) ) ENGINE = InnoDB;");
+    $mysqli->query("CREATE TABLE IF NOT EXISTS `". $chat_id ."_leave`(`id` VarChar( 255 ) NOT NULL, CONSTRAINT `unique_id` UNIQUE( `id` ) ) ENGINE = InnoDB;");
+    $mysqli->query("CREATE TABLE IF NOT EXISTS `". $chat_id ."_settings`(`autokick` TinyInt( 1 ) NOT NULL DEFAULT 0, `greeting` VarChar( 255 ) NULL, `tracking` VarChar( 255 ) NULL, `predsvarn` VarChar( 255 ) NOT NULL DEFAULT 'kick:10', `autoremovepred` Int( 255 ) NOT NULL ) ENGINE = InnoDB;");
+    $mysqli->query("CREATE TABLE IF NOT EXISTS `". $chat_id ."_moders_limit`(`rang` VarChar( 255 ) NOT NULL, `pred` Int( 255 ) NULL, `kick` Int( 255 ) NULL, `tempban` Int( 255 ) NULL) ENGINE = InnoDB;");
+
+    $mysqli->query("INSERT INTO `". $chat_id ."_moders_limit` (`rang`, `pred`) VALUES (1, 5)");
+    $mysqli->query("INSERT INTO `". $chat_id ."_moders_limit` (`rang`, `pred`, `kick`) VALUES (2, 6, 2)");
+    $mysqli->query("INSERT INTO `". $chat_id ."_moders_limit` (`rang`, `pred`, `kick`, `tempban`) VALUES (3, 10, 4, 2)");
+    $mysqli->query("INSERT INTO `". $chat_id ."_settings` (`autoremovepred`) VALUES (". time() + 2419200 .")");
+}//2419200
 ?>
