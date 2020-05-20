f80f321976e137b0b22d5b1c165c9f612fba16f1
kos234 <kostyaperfiliev94@gmail.com>
2020-05-20 23:29:33 +0800

    start

--- web/index.php
+++ web/index.php
@@ -1,25 +1,63 @@
 <?php
 
-require('../vendor/autoload.php');
+use VK\Client\VKApiClient;
 
-$app = new Silex\Application();
-$app['debug'] = true;
+define("CONFIRMATION_TOKEN_VK_BOT", getenv("CONFIRMATION_TOKEN_VK_BOT")); //подтверждение
+define("TOKEN_VK_BOT", getenv("TOKEN_VK_BOT")); //Ключ доступа сообщества
+define("SECRET_KEY_VK_BOT", getenv("SECRET_KEY_VK_BOT")); //Secret key
+define("VERSION_API_VK", 5.103); //Версия апи
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+ini_set('max_execution_time', 900);
 
-// Our web handlers
+if (!isset($_REQUEST)) //проверяем получили ли мы запрос
+    return;
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+$url=parse_url(getenv("CLEARDB_DATABASE_URL")); //Подключаемся к бд
 
-$app->run();
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1); //база данных
+error_log($server.' <- сервер '.$username.' <- имя пользователя '.$password.' <- пароль '.$db.' <- база данных'); //Если нужно узнать данные бд
+$mysqli = new mysqli($server, $username, $password,$db); //Подключаемся
+
+if ($mysqli->connect_error) {//проверка подключились ли мы
+    die('Ошибка подключения (' . $mysqli->connect_errno . ') '. $mysqli->connect_error); //если нет выводим ошибку и выходим из кода
+}
+$mysqli->query("SET NAMES 'utf8'");
+
+$data = json_decode(file_get_contents('php://input'));
+
+//Проверяем secretKey
+if(strcmp($data->secret, SECRET_KEY_VK_BOT) !== 0 && strcmp($data->type, 'confirmation') !== 0)
+    return;//Если не наш, выдаем ошибку серверу vk
+
+//Проверка события запроса
+switch ($data->type) {
+
+    case 'confirmation':
+        //Отправляем код
+        echo CONFIRMATION_TOKEN_VK_BOT;
+        break;
+
+        case 'message_new':
+            $request_params = array(
+                'message' => "" , //сообщение
+                'access_token' => TOKEN_VK_BOT, //токен для отправки от имени сообщества
+                'peer_id' => $data->object->message->peer_id, //айди чата
+                'random_id' => 0, //0 - не рассылка
+                'read_state' => 1,
+                'user_ids' => 0, // Нет конкретного пользователя кому адресованно сообщение
+                'v' => VERSION_API_VK, //Версия API Vk
+                'attachment' => '' //Вложение
+            );
+
+            $vk = new VKApiClient();
+
+        $vk->messages()->send(TOKEN_VK_BOT, $request_params);
+        break;
+
+}
+echo "ok";
+?>
