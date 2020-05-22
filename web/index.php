c7246fe01a36544b06185d6e980ef6c45a8d8c16
kos234 <kostyaperfiliev94@gmail.com>
2020-05-22 17:06:42 +0800

    bugFix

--- web/index.php
+++ web/index.php
@@ -110,7 +110,7 @@ switch ($data->type) {
                         $id = $data->object->message->reply_message->from_id;
                      else
                         $id = substr(explode("|",$text[2])[0], 3);
-                $res_user = json_decode(json_encode($vk->users()->get(SERVICE_KEY, array("user_ids" => $id,
+                $res_user = json_decode(json_encode($vk->users()->get(TOKEN_VK_BOT, array("user_ids" => $id,
                     "fields" => "id,first_name,last_name,deactivated,is_closed,verified,domain,bdate,can_post,can_see_all_posts,can_send_friend_request,"
                 . "can_write_private_message,city,connections,country,contacts,counters,about,activities,education,career,last_seen,interests,home_town,games,has_photo", "name_case" => "abl"))));
                     ob_start();
